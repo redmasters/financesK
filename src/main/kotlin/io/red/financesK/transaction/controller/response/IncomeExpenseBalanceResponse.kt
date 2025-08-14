6ec02141d039ce0079ebf77432a553c24cbefa15
Erasmo Jr. <erasmo.cjunior@gmail.com>
2025-08-14 17:16:06 -0300

    refact: classes sem uso deletadas

--- src/main/kotlin/io/red/financesK/transaction/controller/response/IncomeExpenseBalanceResponse.kt
+++
@@ -1,9 +0,0 @@
-package io.red.financesK.transaction.controller.response
-
-import java.math.BigDecimal
-
-data class IncomeExpenseBalanceResponse(
-    val totalIncome: BigDecimal,
-    val totalExpense: BigDecimal,
-    val balance: BigDecimal // income - expense
-)
